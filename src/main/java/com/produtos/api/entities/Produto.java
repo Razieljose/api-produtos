96f60c8dec06d82a8dd6292af8df8b9fb44522f5
SadAlchemist <raziel_jose@hotmail.com>
2022-11-24 14:52:49 -0300

    Ajeitando código

--- src/main/java/com/produtos/api/entities/Produto.java
+++ src/main/java/com/produtos/api/entities/Produto.java
@@ -1,11 +1,19 @@
 package com.produtos.api.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "tb_produto")
 public class Produto {
@@ -22,48 +30,6 @@ public class Produto {
 	
 	
 	private Long quantidade;
-
-
-	public long getId() {
-		return id;
-	}
-
-
-	public void setId(long id) {
-		this.id = id;
-	}
-
-
-	public String getNome() {
-		return nome;
-	}
-
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-
-	public String getDescricao() {
-		return descricao;
-	}
-
-
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
-
-
-	public Long getQuantidade() {
-		return quantidade;
-	}
-
-
-	public void setQuantidade(Long quantidade) {
-		this.quantidade = quantidade;
-	}
-
-	
 
 }
 
--- src/main/java/com/produtos/api/entities/Vendedor.java
+++ src/main/java/com/produtos/api/entities/Vendedor.java
@@ -1,11 +1,17 @@
 package com.produtos.api.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "tb_vendedor")
 public class Vendedor {
@@ -18,27 +24,4 @@ public class Vendedor {
 
     private String setor;
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getSetor() {
-        return setor;
-    }
-
-    public void setSetor(String setor) {
-        this.setor = setor;
-    }
 }
