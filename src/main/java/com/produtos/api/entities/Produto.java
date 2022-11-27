282ca7143fa607784f88486dbefaa11f4480e3fb
raziel.araujo <raziel_jose@hotmail.com>
2022-11-27 17:23:08 -0300

    Adicionado @Column e @NotEmpty nas entidades que estavam faltando

--- src/main/java/com/produtos/api/entities/Produto.java
+++ src/main/java/com/produtos/api/entities/Produto.java
@@ -5,11 +5,9 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -22,13 +20,15 @@ public class Produto {
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
 	
-	
+	@Column
+	@NotEmpty
 	private String nome;
 	
 	
 	private String descricao;
 	
-	
+	@Column
+	@NotEmpty
 	private Long quantidade;
 
 }
--- src/main/java/com/produtos/api/entities/Vendedor.java
+++ src/main/java/com/produtos/api/entities/Vendedor.java
@@ -5,11 +5,9 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -22,8 +20,12 @@ public class Vendedor {
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
 
+    @Column
+    @NotEmpty
     private String nome;
-
+    
+    @Column
+    @NotEmpty
     private String setor;
 
 }
