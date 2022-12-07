9e4d3b849429c792eebc44d0d2525f6409b305d0
raziel.araujo <raziel_jose@hotmail.com>
2022-12-07 10:29:54 -0300

    Removido configuração http do banco de dados em memória (h2).

--- src/main/java/com/produtos/api/config/SecurityConfig.java
+++ src/main/java/com/produtos/api/config/SecurityConfig.java
@@ -44,8 +44,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .hasAnyRole("USER", "ADMIN")
                 .antMatchers("/api/vendedor/**")
                 .hasAnyRole("USER", "ADMIN")
-                .antMatchers("/h2-console/**")
-                .permitAll()
                 .antMatchers(HttpMethod.POST, "/api/usuario/**")
                 .permitAll()
                 .anyRequest()
